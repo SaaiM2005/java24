23414f1f126ed69673c6b03f42be2c059349e2ea
Sai Mahajan <160035712+SaaiM2005@users.noreply.github.com>
2024-08-28 06:36:18 -0700

    Add files via upload

---
+++ Thread4.java
@@ -0,0 +1,40 @@
+class MyClass implements Runnable
+{
+    int no;
+    String nm;
+    MyClass(int a,String b)
+    {
+        no=a;
+        nm=b;
+    }
+    public void run()
+    {
+        System.out.println("Thread"+nm+" ");
+        for(int i=1;i<=no;i++)
+        {
+            System.out.println(nm+" "+i);
+            try
+            {
+                Thread.sleep(100);
+            }catch(Exception e1){}
+        }
+    }
+}
+public class Thread4
+{
+    public static void main(String[] args) 
+    {
+        Thread a=new Thread(new MyClass(10,"A"));
+        Thread b=new Thread(new MyClass(11, "B"));
+        
+        a.start();
+        b.start();
+        try
+        {
+            a.join();
+            b.join();
+        }catch(Exception e){}
+        System.out.println("Main Ends");
+            
+    }
+}
