5009a486cf26eb4c743edfd1fa30e4c276bbe040
Sai Mahajan <160035712+SaaiM2005@users.noreply.github.com>
2024-10-19 19:43:58 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+public class Test1
+{
+    public static void main(String [] args)
+    {
+        try
+        {
+            // Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/data24", "root","");
+            Statement st=con.createStatement();
+            
+            String sql="create table Trade (tid int primary key ,tnm varchar(20))";
+            st.executeUpdate(sql);
+            System.out.println("Create Table");
+        }
+        catch(Exception e)
+        {
+            System.out.println(""+e);
+        }
+    }
+}
